3890f83a2a99678e82bdf42b8129eb0471df0293
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-11-29 17:31:30 +0700

    web

--- lab6/routes/brand.js
+++ lab6/routes/brand.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var BrandModel = require('../models/BrandModel');
+var MobileModel = require('../models/MobileModel');
 
 router.get('/', async (req, res) => {
    var brands = await BrandModel.find({});
    res.render('brand/index', { brands });
+})
+
+router.get('/add', (req, res) => {
+   res.render('brand/add');
+})
+
+router.post('/add', async (req, res) => {
+   var brand = req.body;
+   await BrandModel.create(brand);
+   res.redirect('/brand');
+})
+
+router.get('/detail/:id', async (req, res) => {
+   var id = req.params.id;
+   //SQL: SELECT * FROM mobiles WHERE brand = "id"
+   var mobiles = await MobileModel.find({ brand : id }).populate('brand');
+   res.render('brand/detail', { mobiles })
 })
 
 module.exports = router;
--- lab6/routes/mobile.js
+++ lab6/routes/mobile.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var MobileModel = require('../models/MobileModel');
+var BrandModel = require('../models/BrandModel');
 
 //URL: localhost:3001/mobile
 router.get('/', async (req, res) => {
    var mobiles = await MobileModel.find({}).populate('brand');
    //Path: views/mobile/index.hbs
    res.render('mobile/index', { mobiles });
+})
+
+router.get('/add', async (req, res) => {
+   var brands = await BrandModel.find({});
+   res.render('mobile/add', { brands });
+})
+
+router.post('/add', async (req, res) => {
+   var mobile = req.body;
+   await MobileModel.create(mobile);
+   res.redirect('/mobile');
 })
 
 module.exports = router;
