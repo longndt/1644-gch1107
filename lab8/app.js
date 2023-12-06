3cb32f718afcea8637d5178b50ed79277e9d6195
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-12-06 15:33:31 +0700

    api

--- lab8/app.js
+++ lab8/app.js
@@ -7,17 +7,74 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 //READ ALL
-app.get('/student', async (req, res) => {
+app.get('/mobile', async (req, res) => {
    try {
-      const collectionName = collection(db, 'students');
-      const collectionData = await getDocs(collectionName);
-      const data = collectionData.docs.map(doc => doc.data());
-      res.send(data);
+      const mobilesRef = collection(db, 'mobiles');  //notes: "mobiles" is collection (table) name
+      const mobilesSnapshot = await getDocs(mobilesRef);
+      const mobilesList = mobilesSnapshot.docs.map(doc => doc.data());
+      res.send(mobilesList);
    } catch (error) {
       console.error(error);
-      res.status(500).send("Error fetching students data");
+      res.status(500).send("Error fetching mobiles data");
+   }
+});
+
+//READ BY ID
+app.get('/mobile/:id', async (req, res) => {
+   try {
+      const { id } = req.params;
+      const docRef = doc(db, 'mobiles', id);
+      const mobileSnapshot = await getDoc(docRef);
+      if (mobileSnapshot.exists()) {
+         const mobile = mobileSnapshot.data();
+         res.send(mobile);
+      } else {
+         res.status(404).send("Mobile data not found");
+      }
+   } catch (error) {
+      console.error("An error occurred:", error);
+      res.status(500).send("Error fetching mobile data: " + error.message);
+   }
+});
+
+//CREATE
+app.post('/mobile', async (req, res) => {
+   try {
+      const newMobile = req.body;
+      const newDocRef = doc(collection(db, 'mobiles'));
+      await setDoc(newDocRef, newMobile);
+      res.status(201).send({ id: newDocRef.id, ...newMobile });
+   } catch (error) {
+      console.error(error);
+      res.status(400).send("Error creating a new mobile");
+   }
+});
+
+//UPDATE
+app.put('/mobile/:id', async (req, res) => {
+   try {
+      const { id } = req.params;
+      const updatedData = req.body;
+      const docRef = doc(db, 'mobiles', id);
+      await updateDoc(docRef, updatedData);
+      res.send(`Mobile with ID ${id} updated`);
+   } catch (error) {
+      console.error(error);
+      res.status(400).send("Error updating mobile");
+   }
+});
+
+//DELETE
+app.delete('/mobile/:id', async (req, res) => {
+   try {
+      const { id } = req.params;
+      const docRef = doc(db, 'mobiles', id);
+      await deleteDoc(docRef);
+      res.send(`Mobile with ID ${id} deleted`);
+   } catch (error) {
+      console.error(error);
+      res.status(400).send("Error deleting mobile");
    }
 });
 
--- lab8/config.js
+++ lab8/config.js
@@ -1,14 +1,15 @@
 const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 
+//set firebase database parameters
 const firebaseConfig = {
-   apiKey: "AIzaSyDTXAWuADPkv9zfcuGjj7F33ZE-Z5qI4zg",
-   authDomain: "gch1107-fd7e0.firebaseapp.com",
-   projectId: "gch1107-fd7e0",
-   storageBucket: "gch1107-fd7e0.appspot.com",
-   messagingSenderId: "192756362203",
-   appId: "1:192756362203:web:2efd0404c3e9179b2955fe",
-   measurementId: "G-FM86Z9DMWB"
+   apiKey: "AIzaSyDp-jmlxzB6rQQbPY6Z5iCgtWnmKcgUXbU",
+   authDomain: "greenwich-62716.firebaseapp.com",
+   projectId: "greenwich-62716",
+   storageBucket: "greenwich-62716.appspot.com",
+   messagingSenderId: "1048233113259",
+   appId: "1:1048233113259:web:94c91d556f3be524afe9ee",
+   measurementId: "G-J841RNLHBL"
 };
 
 const app = initializeApp(firebaseConfig);
